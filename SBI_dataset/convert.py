fd95bb56703fb9d12e933504567ab0bfd0f4f54c
Joh Dokler <joh.dokler@gmail.com>
2022-07-15 13:26:32 +0200

    WIP birth and death

--- SBI_dataset/convert.py
+++ SBI_dataset/convert.py
@@ -10,11 +10,12 @@ Options:
     --count=<count> Limit the number of people to convert.
 """
 
+import urllib
 from lxml import etree
 from docopt import docopt
+from decimal import Decimal
 from dataclasses import dataclass
 from rdflib import Graph, Literal, Namespace, URIRef
-import urllib
 
 
 def main(file_name, output_file_name, count=None):
@@ -23,30 +24,8 @@ def main(file_name, output_file_name, count=None):
     graph.serialize(destination=output_file_name, format='turtle')
 
 
-def load_people(file_name, count=None):
-    """Load data from the provided URL or file, pase it and convert it to the Person class."""
-
-    if is_url(file_name):
-        xml = etree.fromstring(urllib.request.urlopen(file_name).read())
-    else:
-        xml = etree.parse(file_name)
-
-    people = list(xpath_element(xml, '//tei:text/tei:body/tei:listPerson/tei:person[@role="main"]'))
-    if count:
-        people = people[:int(count)]
-
-    for xml_person in people:
-        persName = xpath_element(xml_person, 'tei:persName')[0]
-        person = Person(
-            id=xml_person.get('{http://www.w3.org/XML/1998/namespace}id'),
-            name=xpath_value(persName, 'tei:name'),
-            first_name=xpath_value(persName, 'tei:forename'),
-            last_name=xpath_value(persName, 'tei:surname'),
-            gender=parse_gender(xml_person),
-        )
-
-        yield person
-
+# -----------------------------------------------------------------------------
+# Create the InTavia RDF graph from the available people data.
 
 def create_graph(people):
     """Create RDF graph of the provided people."""
@@ -111,6 +90,27 @@ def create_graph(people):
             g.add((URIRef(name_uri(f'4/{person.id}')), Namespaces.rdfs.label, Literal(person.name)))
             g.add((URIRef(name_uri(f'4/{person.id}')), Namespaces.crm.P2_has_type, Namespaces.idm_nametype.name))
 
+        # ---------------------------------------------------------------------
+        # Birth and death
+
+        for birth in person.birth:
+            birth_event_uri = URIRef(f'{Namespaces.intavia_sbi}birthevent/{person.id}')
+            g.add((birth_event_uri, Namespaces.rdf.type, Namespaces.crm.E67_Birth))
+            g.add((birth_event_uri, Namespaces.crm.P98_brought_into_life, person_proxy_uri))
+            if birth.date:
+                g.add((birth_event_uri, Namespaces.crm["P4_has_time-span"], URIRef(f'{Namespaces.intavia_sbi}timespan/birth/{person.id}')))
+            if birth.place:
+                g.add((birth_event_uri, Namespaces.crm.P7_took_place_at, URIRef(f'{Namespaces.intavia_sbi}placeproxy/TODO-PLACE-ID')))
+
+        for death in person.death:
+            death_event_uri = URIRef(f'{Namespaces.intavia_sbi}deathevent/{person.id}')
+            g.add((death_event_uri, Namespaces.rdf.type, Namespaces.crm.E69_Death))
+            g.add((death_event_uri, Namespaces.crm.P100_was_death_of, person_proxy_uri))
+            if death.date:
+                g.add((death_event_uri, Namespaces.crm["P4_has_time-span"], URIRef(f'{Namespaces.intavia_sbi}timespan/death/{person.id}/1')))
+            if death.place:
+                g.add((death_event_uri, Namespaces.crm.P7_took_place_at, URIRef(f'{Namespaces.intavia_sbi}placeproxy/TODO-PLACE-ID')))
+
     # TODO
     # birth and death
     # - date
@@ -121,6 +121,37 @@ def create_graph(people):
     Namespaces.bind_to_graph(g)
 
     return g
+
+
+# -----------------------------------------------------------------------------
+# Load and parse people from the SBI data file.
+
+def load_people(file_name, count=None):
+    """Load data from the provided URL or file, pase it and convert it to the Person class."""
+
+    if is_url(file_name):
+        xml = etree.fromstring(urllib.request.urlopen(file_name).read())
+    else:
+        xml = etree.parse(file_name)
+
+    people = list(xpath_element(xml, '//tei:text/tei:body/tei:listPerson/tei:person[@role="main"]'))
+    if count:
+        people = people[:int(count)]
+
+    for xml_person in people:
+        persName = xpath_element(xml_person, 'tei:persName')[0]
+
+        person = Person(
+            id=xml_person.get('{http://www.w3.org/XML/1998/namespace}id'),
+            name=xpath_value(persName, 'tei:name'),
+            first_name=xpath_value(persName, 'tei:forename'),
+            last_name=xpath_value(persName, 'tei:surname'),
+            gender=parse_gender(xml_person),
+            birth=list(parse_events(xpath_element(xml_person, 'tei:birth'))),
+            death=list(parse_events(xpath_element(xml_person, 'tei:death'))),
+        )
+
+        yield person
 
 
 # -----------------------------------------------------------------------------
@@ -138,6 +169,14 @@ def xpath_value(element, xpath):
         return None
 
 
+def is_url(string):
+    try:
+        result = urllib.parse.urlparse(string)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
 def parse_gender(person):
     try:
         match xpath_element(person, 'tei:sex')[0].get('value'):
@@ -151,12 +190,85 @@ def parse_gender(person):
         return None
 
 
-def is_url(string):
-    try:
-        result = urllib.parse.urlparse(string)
-        return all([result.scheme, result.netloc])
-    except ValueError:
-        return False
+def parse_date(xml_date):
+    if not xml_date:
+        return None
+    else:
+        if not xml_date[0].get('when'):
+            return None
+        else:
+            match xml_date[0].get('when').split('-'):
+                case [year, month, day]:
+                    return Date(year=int(year), month=int(month), day=int(day))
+                case [year, month]:
+                    return Date(year=int(year), month=int(month), day=None)
+                case [year]:
+                    return Date(year=int(year), month=None, day=None)
+
+
+def parse_place(xml_place):
+
+    def parse_place_parts(xml_place):
+        for part in ['tei:settlement', 'tei:region', 'tei:country']:
+            xml_part = xpath_element(xml_place, part)
+            if xml_part:
+                yield xml_part[0].text
+
+    def parse_location(xml_location):
+        if not xml_location:
+            return None
+        else:
+            location = xml_location[0].text.split(' ')
+            return Location(lat=Decimal(location[0]), lng=Decimal(location[1]))
+
+    if not xml_place:
+        return None
+    else:
+        xml_place = xml_place[0]
+
+        name_parts = list(parse_place_parts(xml_place))
+        if not name_parts:
+            name = None
+        else:
+            name = ', '.join(name_parts)
+
+        return Place(
+            name=name,
+            location=parse_location(xpath_element(xml_place, 'tei:geo')),
+        )
+
+
+def parse_events(xml_events):
+    for xml_event in xml_events:
+        yield Event(
+            date=parse_date(xpath_element(xml_event, 'tei:date')),
+            place=parse_place(xpath_element(xml_event, 'tei:placeName')),
+        )
+
+
+@dataclass
+class Date:
+    year: int
+    month: int
+    day: int
+
+
+@dataclass
+class Location:
+    lat: Decimal
+    lng: Decimal
+
+
+@dataclass
+class Place:
+    name: str
+    location: Location
+
+
+@dataclass
+class Event:
+    date: Date
+    place: Place
 
 
 @dataclass
@@ -167,8 +279,8 @@ class Person:
     first_name: str
     last_name: str
     gender: str
-    # birth
-    # death
+    birth: list[Event]
+    death: list[Event]
 
     def __str__(self) -> str:
         if self.name:
@@ -194,11 +306,11 @@ class Namespaces:
     rdf = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
     rdfs = Namespace('http://www.w3.org/2000/01/rdf-schema#')
     owl = Namespace('http://www.w3.org/2002/07/owl#')
-    crm = Namespace('http://www.cidoc-crm.org/cidoc-crm/')
 
     intavia = Namespace('http://www.intavia.eu/')
     intavia_sbi = Namespace('http://www.intavia.eu/sbi/')
 
+    crm = Namespace('http://www.cidoc-crm.org/cidoc-crm/')
     bioc = Namespace('http://www.ldf.fi/schema/bioc/')
 
     idm_core = Namespace('http://www.intavia.eu/idm-core/')
@@ -207,8 +319,8 @@ class Namespaces:
 
     @classmethod
     def bind_to_graph(cls, graph):
-        graph.bind('crm', cls.crm)
         graph.bind('owl', cls.owl)
+        graph.bind('crm', cls.crm)
         graph.bind('bioc', cls.bioc)
         graph.bind('idm_core', cls.idm_core)
         graph.bind('idm_role', cls.idm_role)
